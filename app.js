427f150a9a56c150b7af181d278709a4429e7641
Weeraprawat Saprong <weeraprawatsaprong@gmail.com>
2026-07-02 01:01:39 +0700

    Enable CORS

--- app.js
+++ app.js
@@ -8,10 +8,12 @@ const orderRoutes = require("./routes/orderRoutes");
 const path = require("path");
 const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("./config/swagger");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(logger);
 
 app.get("/health", (req, res) => {
